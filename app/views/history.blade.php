108669532f8bb4bf600fe241174c74d4b4b7dab2
Smiks <sandi.mikus@gmail.com>
2014-01-01 17:59:43 +0100

    history view fix

--- app/views/history.blade.php
+++ app/views/history.blade.php
@@ -2,27 +2,27 @@
 
 @section('content')
 
-	<h1>Zgodovina bitk</h1>
+    <h1>Zgodovina bitk</h1>
 
-	<?php 
-	$cnt=0;
-	?>
-	@if(count($attackedTerritoryData) > 0)
-		<?php $attackedTerritoryData = $attackedTerritoryData[0]; ?>
-	@endif
-	@if(count($attackedTerritoryData) == 0)
-		Zgodovina bitk je prazna.
-	@endif
-	@foreach($quizIDs as $quizID)
-		<?php 
-		$date=$quizDates[$cnt];
-		$solved = ($solvedQuizes[$cnt]) ? " " : " - še nerešeno";
-		?>
-	 	<li> Napad na naselje: 
-	 		{{ $attackedTerritoryData['name'] }} ({{$attackedTerritoryData['pos_x']}},{{$attackedTerritoryData['pos_y']}})
-	 		<br>
-	 	</li>
-	 	<?php $cnt++; ?>
-	@endforeach
+    <?php 
+    $cnt=0;
+    ?>
+    @if(count($attackedTerritoryData) > 0)
+            <?php $attackedTerritoryData = $attackedTerritoryData[0]; ?>
+    @endif
+    @if(count($attackedTerritoryData) == 0)
+            Zgodovina bitk je prazna.
+    @endif
+    @foreach($quizIDs as $quizID)
+            <?php 
+            $date=$quizDates[$cnt];
+            $solved = ($solvedQuizes[$cnt]) ? " " : " - še nerešeno";
+            ?>
+             <li> Napad na naselje: 
+                     {{ $attackedTerritoryData['name'] }} ({{$attackedTerritoryData['pos_x']}},{{$attackedTerritoryData['pos_y']}})
+                     <br>
+                     <a href="quiz/show/{{$quizID}}"> Poročilo bitke - {{ $date}} {{ $solved }}</a></li>
+             <?php $cnt++; ?>
+    @endforeach
 
 @stop
