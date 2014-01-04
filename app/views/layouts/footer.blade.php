cbf89b6bf818b2fec34246a2341691e35f1a6afe
Smiks <sandi.mikus@gmail.com>
2014-01-04 13:35:40 +0100

    dodana copyright letnica

--- app/views/layouts/footer.blade.php
+++ app/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer id="footer" class="row">
     <div class="col-xs-12">
-        <p class="text-center">Copyright © 2013 iZGOK</p>
+        <p class="text-center">Copyright © 2013-<?php echo date("Y"); ?> iZGOK</p>
     </div>
 </footer>
