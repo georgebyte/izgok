d3eff2384819e93e482e76809a0d456cd5ad0171
Smiks <sandi.mikus@gmail.com>
2014-01-05 20:40:44 +0100

    refactoring profile view-a

--- app/views/profile.blade.php
+++ app/views/profile.blade.php
@@ -2,55 +2,55 @@
 
 @section('content')
 
-
-
 	<div class="profile">
-		<h1>Statistika</h1>
-		<h3>{{ $na }}</h3>
-		<?php
-		$aas=number_format($aas,1);
-		$ads=number_format($ads,1);
-		?>
-		<img src='{{asset($im)}}' alt='user image' width='200' height='200'>
 		
-		</br><br/>
-		<strong>Število rešenih kvizov: {{ $qc }}</strong> <br/><br/>
-		<strong>Največ pravilno odgovorjenih vprašanj v napadu: {{ $hsa }}</strong> <br/><br/>
-		<strong>Največ pravilno odgovorjenih vprašanj v obrambi: {{ $hsd }}</strong> <br/><br/>
-		<strong>Povprečno število pravilno odgovorjenih vprašanj v napadu: {{ $aas }}</strong> <br/><br/>
-		<strong>Povprečno število pravilno odgovorjenih vprašanj v obrambi: {{ $ads }}</strong> <br/><br/>
-		<strong>Število ozemelj: {{ $tc }}</strong> <br/><br/>
-		<strong>Ozemlja:</h4><br/><br/>
-		<div style="padding-left:15px">
-		@foreach($t as $val)
-			<li>Ime: {{ $val['name'] }} <br/>
-		 		Opis: {{ $val['description'] }} <br/>
-		 		koordinate: {{ $val['pos_x']}}, {{ $val['pos_y'] }} <br>
-		 		&nbsp;&nbsp;<a href="/map/show/{{ $val['pos_x'] }}/{{ $val['pos_y'] }}">Pojdi na mapo</a>
-		 		<hr>
-			</li>
-		@endforeach
-		@if(count($t) == 0)
-			Ta igralec nima nobenega ozemlja
-		@else
-			Ta igralec ima {{ count($t) }}
-			@if(count($t) == 1)
-				ozemlje
+		<?php
+		$avgAttackScore=number_format($aas,1);
+		$avgDefenseScore=number_format($ads,1);
+		?>
+		<div style="padding:20px; background-color:#eeeeee; border-radius:20px; border-collapse:separate">
+			<h3>{{ $na }}</h3>
+			<strong>Profilna slika:</strong><br>
+			<img src='{{asset($im)}}' alt='user image' width='200' height='200'>
+			
+			</br><br/>
+			<strong>Statistika:</strong><br>
+			Število rešenih kvizov: <strong>{{ $qc }}</strong> <br/>
+			Največ pravilno odgovorjenih vprašanj v napadu: <strong>{{ $hsa }}</strong> <br/>
+			Največ pravilno odgovorjenih vprašanj v obrambi: <strong>{{ $hsd }}</strong> <br/>
+			Povprečno število pravilno odgovorjenih vprašanj v napadu: <strong>{{ $avgAttackScore }}</strong> <br/>
+			Povprečno število pravilno odgovorjenih vprašanj v obrambi: <strong>{{ $avgDefenseScore }}</strong> <br/><br>
+		</div>
+		<br>
+		<div style="padding:20px; background-color:#eeeeee; border-radius:20px; border-collapse:separate">
+			@if($tc == 0)
+				Ta igralec nima nobenega ozemlja
+			@else
+				Ta igralec ima {{ $tc }}
+				@if($tc == 1)
+					ozemlje
+				@endif
+				@if($tc == 2)
+					ozemlji
+				@endif
+				@if($tc == 3 || $tc == 4)
+					ozemlja
+				@endif
+				@if($tc > 4)
+					ozemelj
+				@endif
 			@endif
-			@if(count($t) == 2)
-				ozemlji
-			@endif
-			@if(count($t) == 3 || count($t) == 4)
-				ozemlja
-			@endif
-			@if(count($t) > 4)
-				ozemelj
-			@endif
-		@endif
+			<br/><br/>
+			<div style="padding-left:15px">
+				@foreach($t as $val)
+					<li>Ime: {{ $val['name'] }} <br/>
+				 		&nbsp;&nbsp;&nbsp;Opis: {{ $val['description'] }} <br/>
+				 		&nbsp;&nbsp;&nbsp;koordinate: {{ $val['pos_x']}}, {{ $val['pos_y'] }} <br>
+				 		&nbsp;&nbsp;&nbsp;<a href="/map/show/{{ $val['pos_x'] }}/{{ $val['pos_y'] }}">Pojdi na mapo</a>
+				 		<hr>
+					</li>
+				@endforeach
+			</div>
 		</div>
 	</div>
-
-
-
-
 @stop
