af19b50a4fba50d707293ea1ecf324f8c08fefc5
matej <matejmart@gmail.comcd>
2014-01-08 17:41:39 +0100

    home page- dodana vsebina

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -2,26 +2,19 @@
 
 @section('content')
     <h1>iZGOK - igrivi zgodovinski kviz</h1>
-    <p>Didakticna spletna igra za ucenje zgodovine.</p>
-    <p>
-    	Linki v uporabi:
-    	<ul>
-    		<li>
-    			/history
-    		</li>
-    		<li>
-    			/auth/register
-    		</li>
-    		<li>
-    			/auth/login
-    		</li>
-            <li>
-                /map
-            </li>
-            <li>
-                /admin/form
-            </li>            
-    	</ul>
+    
+    
+    	
+        <div>
+        <div class="song-home clearfix"><div class="first-letter">I</div><div class="other-letters">grivo se podaj v učenje zgodovine,</div></div>
+        <div class="song-home clearfix"><div class="first-letter">Z</div><div class="other-letters">aradi šole naj veselje te ne mine!</div></div>
+        <div class="song-home clearfix"><div class="first-letter">G</div><div class="other-letters">ospoduj nad soigralci, pokaži kaj znaš.</div></div>
+        <div class="song-home clearfix"><div class="first-letter">O</div><div class="other-letters">svoji ozemlje, sosedovo vas.</div></div>
+        <div class="song-home clearfix"><div class="first-letter">K</div><div class="other-letters">ot vladar se podaj skoz' zgodovinski čas!  </div></div>
+    </div>
+    <br>
 
-    </p>
+    <p>IZGOK je didaktična spletna igra za učenje zgodovine, ki suhoparno učenje zgodovinskih letnic in dejstev spreminja v zabavno igralsko izkušnjo. Aplikacija je namenjena srednješolcem in osnovnošolcem, ki si želijo popestriti učenje zgodovine in pa vsem navdušencem nad spletnimi igrami z več igralci, ki jih zgodovina zanima. Igra temelji na principu, da je znanje moč, poznavanje zgodovine pa igralcu služi kot orožje za osvajanje novih ozemelj. Na ta način igralec širi svoje znanje na zabaven in interaktiven način ter gradi imperij v boju s soigralci.</p>
+
+    
 @stop
