8de00c19358abb2672a6e7fb656967e8f36371f9
Jure Bajt <jure.bajt@gmail.com>
2014-01-08 17:01:03 +0100

    Popravki designa

--- app/views/auth/login.blade.php
+++ app/views/auth/login.blade.php
@@ -1,44 +1,46 @@
 @extends('layouts.body')
 
-@section('content')
-    {{ Form::open(array(
-        'url'   => 'auth/login',
-        'class' => 'col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3'
-    )) }}
+@section('content_container')
+    <section id="main-content" class="col-sm-12">
+        {{ Form::open(array(
+            'url'   => 'auth/login',
+            'class' => 'col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3'
+        )) }}
 
-        <div class="form-group">
-            {{ Form::label('username', 'Uporabniško ime') }}
-            {{ Form::text('username', '', array(
-                'class'       => 'form-control',
-                'placeholder' => 'Uporabniško ime'
-            )) }}
-        </div>
+            <div class="form-group">
+                {{ Form::label('username', 'Uporabniško ime') }}
+                {{ Form::text('username', '', array(
+                    'class'       => 'form-control',
+                    'placeholder' => 'Uporabniško ime'
+                )) }}
+            </div>
 
-        <div class="form-group">
-            {{ Form::label('password', 'Geslo') }}
-            {{ Form::password('password', array(
-                'class'       => 'form-control',
-                'placeholder' => 'Geslo'
-            )) }}
-        </div>
+            <div class="form-group">
+                {{ Form::label('password', 'Geslo') }}
+                {{ Form::password('password', array(
+                    'class'       => 'form-control',
+                    'placeholder' => 'Geslo'
+                )) }}
+            </div>
 
-        <div class="checkbox">
-            {{ Form::label('remember_me', 'Zapomni si me') }}
-            {{ Form::checkbox('remember_me', 'on', true) }}
-        </div>
-        
+            <div class="checkbox">
+                {{ Form::label('remember_me', 'Zapomni si me') }}
+                {{ Form::checkbox('remember_me', 'on', true) }}
+            </div>
+            
 
 
 
-        <div class="form-group @if(Session::has('error'))has-error @endif">
-            {{ Form::button('Login', array(
-                'type'  => 'submit',
-                'class' => 'btn btn-primary'
-            )) }}
-            @if(Session::has('error'))
-                <p class="help-block">{{ Session::get('error') }}</p>
-            @endif
-        </div>
+            <div class="form-group @if(Session::has('error'))has-error @endif">
+                {{ Form::button('Prijava', array(
+                    'type'  => 'submit',
+                    'class' => 'btn btn-primary'
+                )) }}
+                @if(Session::has('error'))
+                    <p class="help-block">{{ Session::get('error') }}</p>
+                @endif
+            </div>
 
-    {{ Form::close() }}
+        {{ Form::close() }}
+    </section>
 @stop
--- app/views/auth/register.blade.php
+++ app/views/auth/register.blade.php
@@ -1,90 +1,92 @@
 @extends('layouts.body')
 
-@section('content')
-    {{ Form::open(array(
-        'url'   => 'auth/register',
-        'class' => 'col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3',
-        //za dodajanje slik uporabnikov
-        'files' => true
-    )) }}
-
-        <div class="form-group @if($errors->has('username'))has-error @elseif(!$errors->has('username') && $errors->all())has-success @endif">
-            {{ Form::label('username', 'Uporabniško ime', array(
-                'class' => 'control-label'
-            )) }}
-            {{ Form::text('username', '', array(
-                'class'       => 'form-control',
-                'placeholder' => 'Uporabniško ime'
-            )) }}
-            {{ $errors->first('username', '<p class="help-block">:message</p>') }}
-        </div>
-
-        <div class="form-group @if($errors->has('email'))has-error @elseif(!$errors->has('email') && $errors->all())has-success @endif">
-            {{ Form::label('email', 'Elektronski naslov', array(
-                'class' => 'control-label'
-            )) }}
-            {{ Form::email('email', '', array(
-                'class'       => 'form-control',
-                'placeholder' => 'Elektronski naslov'
-            )) }}
-            {{ $errors->first('email', '<p class="help-block">:message</p>') }}
-        </div>
-
-        <div class="form-group @if($errors->has('password'))has-error @endif">
-            {{ Form::label('password', 'Geslo', array(
-                'class' => 'control-label'
-            )) }}
-            {{ Form::password('password', array(
-                'class'       => 'form-control',
-                'placeholder' => 'Geslo'
-            )) }}
-            {{ $errors->first('password', '<p class="help-block">:message</p>') }}
-        </div>
-
-        <div class="form-group @if($errors->has('password'))has-error @endif">
-            {{ Form::label('password_confirmation', 'Potrdi geslo', array(
-                'class' => 'control-label'
-            )) }}
-            {{ Form::password('password_confirmation', array(
-                'class'       => 'form-control',
-                'placeholder' => 'Potrdi geslo'
-            )) }}
-        </div>
-        
-        <div class="form-group @if($errors->has('slika'))has-error @endif">
-            {{ Form::label('slika', 'Slika uporabnika', array(
-                'class' => 'control-label'
-            )) }}
-            {{ Form::file('slika', '', array(
-                'class' => 'form-control'
-                
-            )) }}
-            {{ $errors->first('slika', '<p class="help-block">:message</p>') }}
-        </div>
-        
-        <div class="form-group">
-                {{ Form::label("location", "Lokacija na mapi") }}
-                <br>
-                {{ Form::radio("location", 'NE', true) }} NE
-                &nbsp;
-                {{ Form::radio("location", 'SE') }} SE
-                &nbsp;
-                {{ Form::radio("location", 'SW') }} SW
-                &nbsp;
-                {{ Form::radio("location", 'NW') }} NW
-        </div>
-
-       
-
-        {{ Form::button('Registracija', array(
-            'type'  => 'submit',
-            'class' => 'btn btn-primary'
+@section('content_container')
+    <section id="main-content" class="col-sm-12">
+        {{ Form::open(array(
+            'url'   => 'auth/register',
+            'class' => 'col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3',
+            //za dodajanje slik uporabnikov
+            'files' => true
         )) }}
 
-        {{ Form::button('Počisti', array(
-            'type'  => 'reset',
-            'class' => 'btn btn-danger'
-        )) }}
+            <div class="form-group @if($errors->has('username'))has-error @elseif(!$errors->has('username') && $errors->all())has-success @endif">
+                {{ Form::label('username', 'Uporabniško ime', array(
+                    'class' => 'control-label'
+                )) }}
+                {{ Form::text('username', '', array(
+                    'class'       => 'form-control',
+                    'placeholder' => 'Uporabniško ime'
+                )) }}
+                {{ $errors->first('username', '<p class="help-block">:message</p>') }}
+            </div>
 
-    {{ Form::close() }}
+            <div class="form-group @if($errors->has('email'))has-error @elseif(!$errors->has('email') && $errors->all())has-success @endif">
+                {{ Form::label('email', 'Elektronski naslov', array(
+                    'class' => 'control-label'
+                )) }}
+                {{ Form::email('email', '', array(
+                    'class'       => 'form-control',
+                    'placeholder' => 'Elektronski naslov'
+                )) }}
+                {{ $errors->first('email', '<p class="help-block">:message</p>') }}
+            </div>
+
+            <div class="form-group @if($errors->has('password'))has-error @endif">
+                {{ Form::label('password', 'Geslo', array(
+                    'class' => 'control-label'
+                )) }}
+                {{ Form::password('password', array(
+                    'class'       => 'form-control',
+                    'placeholder' => 'Geslo'
+                )) }}
+                {{ $errors->first('password', '<p class="help-block">:message</p>') }}
+            </div>
+
+            <div class="form-group @if($errors->has('password'))has-error @endif">
+                {{ Form::label('password_confirmation', 'Potrdi geslo', array(
+                    'class' => 'control-label'
+                )) }}
+                {{ Form::password('password_confirmation', array(
+                    'class'       => 'form-control',
+                    'placeholder' => 'Potrdi geslo'
+                )) }}
+            </div>
+            
+            <div class="form-group @if($errors->has('slika'))has-error @endif">
+                {{ Form::label('slika', 'Slika uporabnika', array(
+                    'class' => 'control-label'
+                )) }}
+                {{ Form::file('slika', '', array(
+                    'class' => 'form-control'
+                    
+                )) }}
+                {{ $errors->first('slika', '<p class="help-block">:message</p>') }}
+            </div>
+            
+            <div class="form-group">
+                    {{ Form::label("location", "Lokacija na mapi") }}
+                    <br>
+                    {{ Form::radio("location", 'NE', true) }} SV
+                    &nbsp;
+                    {{ Form::radio("location", 'SE') }} JV
+                    &nbsp;
+                    {{ Form::radio("location", 'SW') }} JZ
+                    &nbsp;
+                    {{ Form::radio("location", 'NW') }} SZ
+            </div>
+
+           
+
+            {{ Form::button('Registracija', array(
+                'type'  => 'submit',
+                'class' => 'btn btn-primary'
+            )) }}
+
+            {{ Form::button('Počisti', array(
+                'type'  => 'reset',
+                'class' => 'btn btn-danger'
+            )) }}
+
+        {{ Form::close() }}
+    </section>
 @stop
