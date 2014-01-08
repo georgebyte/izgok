b7f36633ba812310bbdbef1e301770a2422edbf5
Smiks <sandi.mikus@gmail.com>
2014-01-08 15:05:36 +0100

    user auto complete ne prikazuje vec NPCjev

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -9,7 +9,7 @@ class ApiController extends BaseController {
 
     public function getLookup($user = null)
     {
-				$userAllData = User::where('username', 'LIKE', '%'.$user.'%') -> get(array('username'));
+				$userAllData = User::where('username', 'LIKE', '%'.$user.'%') -> where('email', '!=', 'NPC') -> get(array('username'));
 				$tmp = array();
 				foreach($userAllData as $val){
 					array_push($tmp, $val['username']);
