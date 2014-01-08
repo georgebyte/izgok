b86dc40a6f598ccb9ae13e2116d9f5c6725d0468
matej <matejmart@gmail.comcd>
2014-01-08 17:56:50 +0100

    home page-popravek

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.body')
 
 @section('content')
-    <h1>iZGOK - igrivi zgodovinski kviz</h1>
+    
     
     
     	
@@ -13,6 +13,10 @@
         <div class="song-home clearfix"><div class="first-letter">K</div><div class="other-letters">ot vladar se podaj skoz' zgodovinski čas!  </div></div>
     </div>
     <br>
+    <br>
+    <h1>Igrivi zgodovinski kviz</h1>
+
+     <br>
 
     <p>IZGOK je didaktična spletna igra za učenje zgodovine, ki suhoparno učenje zgodovinskih letnic in dejstev spreminja v zabavno igralsko izkušnjo. Aplikacija je namenjena srednješolcem in osnovnošolcem, ki si želijo popestriti učenje zgodovine in pa vsem navdušencem nad spletnimi igrami z več igralci, ki jih zgodovina zanima. Igra temelji na principu, da je znanje moč, poznavanje zgodovine pa igralcu služi kot orožje za osvajanje novih ozemelj. Na ta način igralec širi svoje znanje na zabaven in interaktiven način ter gradi imperij v boju s soigralci.</p>
 
