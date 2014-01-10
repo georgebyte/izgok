5229148936a134af015579797cbda16e0205347b
Jure Bajt <jure.bajt@gmail.com>
2014-01-10 21:50:18 +0100

    RADIIIIII! (Zemljevid sveta 1.0).

--- app/views/world.blade.php
+++ app/views/world.blade.php
@@ -11,12 +11,24 @@
         <?php
         $im = "/map/drawworld";
         ?>
-        <div id="world" class="world"><img src="{{asset($im)}}"></div>
+        <div id="world" class="world"><img id="world-map" src="{{asset($im)}}"></div>
     </div>
 
     @section('footer')
         @include('layouts.footer')
     @show
 </div>
-<script src="/js/World.js"></script>
+<script>
+    $(document).ready(function() {
+        var img = document.getElementById('world'); 
+        var divWidth = img.clientWidth;
+        var divHeight = img.clientHeight;
+        var imageWidth = {{ $xCenter*2 }};
+        var imageHeight = {{ $yCenter*2 }};
+        var scrollTop = Math.floor((imageHeight - divHeight)/2);
+        var scrollLeft = Math.floor((imageWidth - divWidth)/2);
+        $('#world').animate({ scrollTop: scrollTop}, 500);
+        $('#world').animate({ scrollLeft: scrollLeft}, 500);
+    });
+</script>
 @stop
--- public/js/World.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-   $('#world').animate({ scrollTop: 660}, 500);
-   $('#world').animate({ scrollLeft: 275}, 500);
-});
