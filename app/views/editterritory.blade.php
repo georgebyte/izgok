fa033e17db3851de9ea63d9c5221bb66a40a24f2
matej <matejmart@gmail.comcd>
2014-01-10 19:49:16 +0100

    editterritory responsive design

--- app/views/editterritory.blade.php
+++ app/views/editterritory.blade.php
@@ -2,19 +2,45 @@
 
 @section('content')
 
-    <div class="form-group">
-    	<br>
-    	{{ Form::open(array('url' => 'profile/edit/')) }}
-    		{{ Form::hidden('id', $territoryInfo['id'])}}
+    
 
-        	{{ Form::label('name', 'Ime naselja:') }}&nbsp;
-            {{ Form::text('name', $territoryInfo['name'])}}<br>
+    
+        <div id="main-container">
+            {{ Form::open(array(
+                'url'   => 'profile/edit/',
+                'class' => 'form-inline'
+            )) }}
+                <div class="form-group">
+                    {{ Form::hidden('id', $territoryInfo['id'])}}
+                    {{ Form::label('name', 'Ime naselja:') }}&nbsp;
+                    {{ Form::text('name', $territoryInfo['name'], array(
+                            'class'       => 'form-control',
+                            'id'          => 'name'
+                    )) }}
+                    {{ Form::label('description', 'Opis naselja:') }}<br>
+                    {{ Form::textarea('description', $territoryInfo['description'],array(
+                            'class'       => 'form-control',
+                            'id'          => 'description'
+                    )) }}<br>
+                    {{ Form::button('Uredi', array(
+                    'type'  => 'submit',
+                    'class' => 'btn btn-primary scorebord-players-search'
+                )) }}
 
-            {{ Form::label('description', 'Opis naselja:') }}<br>
-            {{ Form::textarea('description', $territoryInfo['description'])}}<br>
+                </div>
+               
+            {{ Form::close() }}
+            
+        @section('footer')
+            @include('layouts.footer')
+        @show
+    
 
-            {{ Form::button('Uredi', array('type' => 'submit', 'class' => 'btn btn-primary')) }}
-    	{{ Form::close() }}
+
+       
     </div>
 
+
 @stop
+
+
