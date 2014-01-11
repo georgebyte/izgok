4c39e3a64ff4966963dab7c5359c39436ddd944b
Smiks <sandi.mikus@gmail.com>
2014-01-11 22:56:03 +0100

    refactoring controlpanel controllerja

--- app/controllers/ControlPanelController.php
+++ app/controllers/ControlPanelController.php
@@ -8,56 +8,47 @@ class ControlPanelController extends BaseController {
         $this->beforeFilter('csrf', array('on' => 'post'));
     }
 
-    
-
     public function getChange()
     {
-        $error=array("up"=>"");
+        $error = array("up" => "");
         return View::make('control',$error);
     }
     
-
     public function postChange()
     {
+        $data = Input::all();
+
+        if(!(Auth::validate(array('username' => Auth::user() -> username, 'password' => $data['oldPassword'])))){ 
+            $error=array("up" => "Username and password combination is not correct.");
+            return View::make('control',$error);
+        }
+
+        $rules = array(
+            'password' => 'confirmed',
+            'slika'    => 'image|between:0,500'
+        );
+
+        $validator = Validator::make($data, $rules);
+
+        if (!($validator->passes())) {
+            return Redirect::to('control') -> withInput() -> withErrors($validator);
+        }
+
+        $userID = Auth::user() -> id;   
         
-        $data=Input::all();
+        if(Input::has('password')){
+            User::where('id', '=', $userID)
+                ->update(array('password'=>Hash::make(Input::get('password'))));
+        }  
 
-        
-        if (Auth::validate(array('username' => Auth::user() -> username, 'password' => $data['oldPassword'])))       
-        {    
-            $rules = array(
-                'password'    => 'confirmed',
-                'slika'       => 'image|between:0,500'
-            );
-
-            $validator = Validator::make($data, $rules);
-
-            if ($validator->passes()) {
-                $userID = Auth::user() -> id;   
-                
-                if(Input::has('password')){
-                    User::where('id', '=', $userID)
-                        ->update(array('password'=>Hash::make(Input::get('password'))));
-                }  
-
-                if(Input::hasFile('slika')){
-                    $file = Input::file('slika');
-                    $fileName = md5($userID).".".$file->getClientOriginalExtension();
-                    $destinationPath = Config::get('auth.usersAvatarsLocation');
-                    $file->move($destinationPath, $fileName);
-
-                    User::where('id', '=', $userID)->update(array('image_path' => $fileName));
-                }
-                
-                return Redirect::to('profile');
-            }
-            
-            return Redirect::to('control')->withInput()->withErrors($validator);
+        if(Input::hasFile('slika')){
+            $file = Input::file('slika');
+            $fileName = md5($userID).".".$file -> getClientOriginalExtension();
+            $destinationPath = Config::get('auth.usersAvatarsLocation');
+            $file -> move($destinationPath, $fileName);
+            User::where('id', '=', $userID) -> update(array('image_path' => $fileName));
         }
         
-        $error=array("up"=>"Username and password combination is not correct.");
-        return View::make('control',$error);
-            
+        return Redirect::to('profile');
     }
-
 }
